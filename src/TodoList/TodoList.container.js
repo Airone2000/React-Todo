e4ca75a6b2f3367ed4570b6446843f26c275f150
Maels1991 <r.wan.guillou+github@gmail.com>
2018-11-28 08:15:31 +0100

    Base TodoListContainer

---
+++ src/TodoList/TodoList.container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoList extends React.Component
+{
+  render() {
+    return (
+      <div className="todolist-container">
+        TODOLIST CONTAINER
+      </div>
+    );
+  }
+}
+
+export default TodoList;
