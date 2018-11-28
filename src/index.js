7d99648add250b6d5dbb2865cb236c3bfcb96b07
Maels1991 <r.wan.guillou+github@gmail.com>
2018-11-28 08:17:12 +0100

    Base style for todo container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {default as TodoContainer} from './TodoList/TodoList.container';
+import './index.css';
 
 ReactDOM.render(
   <TodoContainer />,
